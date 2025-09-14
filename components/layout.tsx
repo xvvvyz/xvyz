968367366ecffdd6be32b23647e76094f413d57c
cade <git@cade.me>
2025-09-14 12:27:56 -0700

    prevent fouc

--- components/layout.tsx
+++ components/layout.tsx
@@ -18,6 +18,11 @@ export const Layout = ({
         <title>{title}</title>
         <link rel="stylesheet" href="/tailwind.css" />
         <Fonts />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `const e=document.documentElement.style;e.visibility='hidden';const s=document.querySelector('link[href="/tailwind.css"]');if(s?.sheet||!s){e.visibility='visible'}else{s.addEventListener('load',()=>e.visibility='visible')}`,
+          }}
+        />
       </head>
       <body class="m-0 flex min-h-dvh w-screen items-center justify-center bg-neutral-50 font-sans text-neutral-900 dark:bg-neutral-900 dark:text-neutral-200">
         {children}
